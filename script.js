f9dc686a036ef137d15d72c031d1974b98bbf1fa
DealeGear <116604988+DealeGear@users.noreply.github.com>
2025-02-13 07:27:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.getElementById("menu-toggle");
   const navMenu = document.getElementById("nav-menu");
+  const navLinks = navMenu.querySelectorAll("a");
 
   menuToggle.addEventListener("click", () => {
     navMenu.classList.toggle("show");
+  });
+
+  // Fecha o menu ao clicar em qualquer subitem
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      if (navMenu.classList.contains("show")) {
+        navMenu.classList.remove("show");
+      }
+    });
   });
 });
